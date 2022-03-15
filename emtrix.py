1b5f6d6c6df47c6f6940fe223f28180d34243453
Braydon Jones <bradofrado@gmail.com>
2022-03-15 17:06:18 -0600

    Cleaned up the operations a bit

--- emtrix.py
+++ emtrix.py
@@ -14,27 +14,30 @@ class ObjectType(Enum):
     Matrix = 0
     Int = 1
 
-def transpose(x):
-    if isMatrix(x) == False:
-        raise Exception("Cannot before the operation transpose on this data type")
-    return np.transpose(x.getValue())
+class Callable:
+    def __init__(self, function):
+        self.function = function
+    def __call__(self, *args, **kwargs):
+        return self.function(*args, **kwargs)
 
-# def row(x):
-#     if isMatrix(x) == False:
-#         raise Exception("Cannot before the operation transpose on this data type")
-#     return row_echelon
+class MatrixCallable(Callable):
+    def __call__(self, *args, **kwds):
+        if isMatrix(args[0]) == False:
+            raise Exception("Cannot perform the operation on this data type")
+        return super().__call__(*args, **kwds)
+
 class OperatorType(Enum):
-    PLUS = lambda x, y: x + y,
-    MINUS = lambda x, y: x - y,
-    MULTIPLY = lambda x, y: x * y,
-    DIVIDE = lambda x, y: x / y,
-    DET = lambda x: x.det(),
-    EIG = lambda x: x.eig(),
-    INV = lambda x: x.inv(),
-    POWER = lambda x,y: x**y,
-    SOLVE = lambda x,y: x.solve(y),
-    ROW = lambda x: x.row(),
-    T = transpose,
+    PLUS = Callable(lambda x, y: x + y)
+    MINUS = Callable(lambda x, y: x - y)
+    MULTIPLY = Callable(lambda x, y: x * y)
+    DIVIDE = Callable(lambda x, y: x / y)
+    DET = MatrixCallable(lambda x: np.linalg.det(x.getValue()))
+    EIG = MatrixCallable(lambda x: np.linalg.eig(x.getValue()))
+    INV = MatrixCallable(lambda x: np.linalg.inv(x.getValue()))
+    POWER = MatrixCallable(lambda x,y: x**y)
+    SOLVE = MatrixCallable(lambda x,y: LU_solver(x.getValue(), y.getValue()))
+    ROW = MatrixCallable(lambda x: row_echelon(x.getValue()))
+    T = MatrixCallable(lambda x: np.transpose(x.getValue()))
 
 class Value():
     def __init__(self):
@@ -69,16 +72,6 @@ class Value():
         if isinstance(other, Value):
             return self.getValue()**other.getValue()
         return self.getValue()**other
-    def eig(self):
-        raise Exception("Cannot perform the operation eig on this data type")
-    def det(self):
-        raise Exception("Cannot perform the operation det on this data type")
-    def inv(self):
-        raise Exception("Cannot perform the operation inv on this data type")
-    def solve(self):
-        raise Exception("Cannot perform the operation solve on this data type")
-    def row(self):
-        raise Exception("Cannot perform the operation row on this data type")
 
 class Number(Value):
     def __init__(self, value):
@@ -129,18 +122,6 @@ class Matrix(Value):
             return np.linalg.inv(self**value)
 
         return super().__pow__(other)
-    def eig(self):
-        return np.linalg.eig(self.value)
-    def det(self):
-        return np.linalg.det(self.value)
-    def inv(self):
-        return np.linalg.inv(self.value)
-    def solve(self, other):
-        if isMatrix(other) == False:
-            raise Exception("Can only solve a matrix with a vector")
-        return LU_solver(self.value, other.getValue())
-    def row(self):
-        return row_echelon(self.value)
 
 class Function(Value):
     def __init__(self, value : Value, value2 : Value, type : OperatorType):
@@ -150,8 +131,8 @@ class Function(Value):
         self.type = type
     def getValue(self):
         if self.value2:
-            return self.type.value[0](self.value, self.value2)
-        return self.type.value[0](self.value)
+            return self.type.value(self.value, self.value2)
+        return self.type.value(self.value)
 
 class Computation(Value):
     def __init__(self, leftValue : Value, rightValue : Value, type : OperatorType):
@@ -160,7 +141,7 @@ class Computation(Value):
         self.rightValue = rightValue
         self.type = type
     def getValue(self):
-        return self.type.value[0](self.leftValue, self.rightValue)
+        return self.type.value(self.leftValue, self.rightValue)
         
     
 class Variable(Value):
